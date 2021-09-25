e8fef2083e5b93dda627d87a768eeb61694d633d
Thanmai Gajam <thanmai.gajam@sjsu.edu>
2021-09-25 11:12:39 -0700

    Formatting in app.js and combines dfs of all sources in main.py flask

--- flaskend/main.py
+++ flaskend/main.py
@@ -129,26 +129,9 @@ def get_textBlob_score(sent):
     polarity = TextBlob(sent).sentiment.polarity
     return polarity
 
-@app.route('/getreviews_reddit/')
-def get_redditreviews():
-    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=head)
-    TOKEN = 'bearer ' + res.json()['access_token']
-    headers = {'Authorization': TOKEN,'User-Agent': 'MyAPI/0.0.1' }
-    res1 = requests.get('https://oauth.reddit.com/r/starbucks/hot?limit='+request.args.get('limitval'), headers=headers)
-    df_title = pd.DataFrame()
-    for post in res1.json()['data']['children']:
-        df_title = df_title.append({
-            # 'subeddit':post['data']['subreddit'],
-            'title':post['data']['title'],
-            # 'selftext':post['data']['selftext'],
-            # 'upvote_ratio':post['data']['upvote_ratio'],
-            # 'ups':post['data']['ups'],
-            # 'downs':post['data']['downs'],
-            # 'num_comments':post['data']['num_comments'],
-            # 'created_utc':post['data']['created_utc'], #Check the date
-            # 'subreddit_subscribers':post['data']['subreddit_subscribers'], 
-        }, ignore_index = True)
-
+def processing(df_title):
+    print("df_title in processing")
+    print(df_title.head())
     df_title.title[df_title.title.str.match(pat = '(https)|(http)|(www.)',na = False)]
     de_emotext = []
     for text in df_title.title:
@@ -311,7 +294,80 @@ def get_redditreviews():
         print(i,") ",df_full.iloc[Most_negative_sentences.index[i]]['title'])
     print("Most_negative_sentences --------------------------", Most_negative_sentences)
 
-    return {"data":"success sent to database"}  
+def getReditData(res1):
+    df_title = pd.DataFrame()
+    for post in res1.json()['data']['children']:
+        df_title = df_title.append({
+            # 'subeddit':post['data']['subreddit'],
+            'title':post['data']['title'],
+            # 'selftext':post['data']['selftext'],
+            # 'upvote_ratio':post['data']['upvote_ratio'],
+            # 'ups':post['data']['ups'],
+            # 'downs':post['data']['downs'],
+            # 'num_comments':post['data']['num_comments'],
+            # 'created_utc':post['data']['created_utc'], #Check the date
+            # 'subreddit_subscribers':post['data']['subreddit_subscribers'], 
+        }, ignore_index = True)
+    return df_title
+
+def getYelpData(result):
+    headers = {
+        'Authorization' : 'bearer '+ config_data['yelp']['token']
+    }
+    df_ids = pd.DataFrame()
+    df_reviews = pd.DataFrame()
+    for post in result.json()['businesses']:
+        df_ids = df_ids.append({
+            'id':post['id']
+        },ignore_index=True)
+    print(df_ids['id'].shape[0])
+    for id_val in range(df_ids['id'].shape[0]):
+        print(df_ids['id'][id_val])
+        idval = df_ids['id'][id_val]
+        time.sleep(1)
+        url = 'https://api.yelp.com/v3/businesses/'+idval+'/reviews'
+        result = requests.get(url,headers=headers)
+        for post in result.json()['reviews']:
+            df_reviews = df_reviews.append({
+                'text':post['text'],
+                'rating': post['rating']
+            },ignore_index=True)
+    return df_reviews
+    
+
+def getTwitterData(results):
+    df_tweet = pd.DataFrame()
+    for post in results.json()['data']:
+        df_tweet = df_tweet.append({
+            'test':post['text'], 
+        }, ignore_index = True)
+    print(df_tweet.head())
+    return df_tweet
+
+@app.route('/get_processed_data/')
+def get_processed_data():
+    data_reddit = requests.get("http://127.0.0.1:5000/getreviews_reddit?limitval=36")
+    reddit_df = getReditData(data_reddit)
+
+    data_yelp = requests.get("http://127.0.0.1:5000/getreviews_yelp?term=Starbucks&location=arizona")
+    yelp_df = getYelpData(data_yelp)
+ 
+    data_twitter = requests.get("http://127.0.0.1:5000/getreviews_twitter?query=Starbucks&tweet.fields=author_id&max_results=10")
+    twitter_df = getTwitterData(data_twitter)
+
+    frames = [df1, df2, df3]
+    result = pd.concat(frames)
+
+    return data_twitter.json()
+
+@app.route('/getreviews_reddit/')
+def get_redditreviews():
+    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=head)
+    TOKEN = 'bearer ' + res.json()['access_token']
+    headers = {'Authorization': TOKEN,'User-Agent': 'MyAPI/0.0.1' }
+    res1 = requests.get('https://oauth.reddit.com/r/starbucks/hot?limit='+request.args.get('limitval'), headers=headers)
+    return res1.json()
+     
 
 # # PYMONGO CONNECTION
 #     client = PyMongo(app,uri = config_data['mongodb_config'])
@@ -339,37 +395,19 @@ def getreviews_yelp():
         'term' : request.args.get('term'),
         'location' : request.args.get('location')
     }
-    print(request.args.get('term'))
-    print(request.args.get('location'))
+    # print(request.args.get('term'))
+    # print(request.args.get('location'))
     url = 'https://api.yelp.com/v3/businesses/search?term='+params['term']+'&location='+params['location']
     print(url)
     result = requests.get(url,headers=headers)
-    df_ids = pd.DataFrame()
-    df_reviews = pd.DataFrame()
-    for post in result.json()['businesses']:
-        df_ids = df_ids.append({
-            'id':post['id']
-        },ignore_index=True)
-    print(df_ids['id'].shape[0])
-    for id_val in range(df_ids['id'].shape[0]):
-        print(df_ids['id'][id_val])
-        idval = df_ids['id'][id_val]
-        time.sleep(1)
-        url = 'https://api.yelp.com/v3/businesses/'+idval+'/reviews'
-        result = requests.get(url,headers=headers)
-        for post in result.json()['reviews']:
-            df_reviews = df_reviews.append({
-                'text':post['text'],
-                'rating': post['rating']
-            },ignore_index=True)
-
-    return df_reviews.to_json()
+    return result.json()
+    
 
 @app.route('/getreviews_twitter/')
 def getreviews_twitter():
-    print(request.args.get("query"))
-    print(request.args.get("tweet.fields"))
-    print(request.args.get("max_results"))
+    # print(request.args.get("query"))
+    # print(request.args.get("tweet.fields"))
+    # print(request.args.get("max_results"))
     headers = {
       "User-Agent": config_data['twitter']['user_agent'],
       "authorization": 'Bearer '+config_data['twitter']['token']
@@ -380,7 +418,6 @@ def getreviews_twitter():
     "max_results": 10,
     }
     results = requests.get('https://api.twitter.com/2/tweets/search/recent',params,headers=headers)
-    print(results.json())
     return results.json()
 
 if __name__ == "__main__":
