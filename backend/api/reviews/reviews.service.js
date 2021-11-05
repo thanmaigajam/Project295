a0d5b7f0f0b8b0f63eb7ee4aa3cc3178089fd6fe
Thanmai Gajam <thanmai.gajam@sjsu.edu>
2021-11-05 16:08:03 -0700

    Fixed bosy and added Linegrpah in model

--- backend/api/reviews/reviews.service.js
+++ backend/api/reviews/reviews.service.js
@@ -1,70 +1,53 @@
 var axios = require("axios");
 
 module.exports = {
-    get_flaskend_service : (body,callBack) => {
-        console.log("In topic rating service", body)
-        let brand = body.brandname;
-        let source = bosy.reviewtype;
+  get_flaskend_service: (body, callBack) => {
+    console.log("In topic rating service", body);
+    let brand = body.brandname;
+    let source = body.reviewtype;
 
-        switch(source){
-            case "yelp" : 
-                axios
-                .get(
-                  "http://127.0.0.1:5000/get_processed_data_yelp?brand=" +
-                    brand +
-                    "&location=arizona"
-                )
-                  .then((res) => {
-                    return callBack(null,res)
-                  })
-                  .catch((ex) => {
-                    console.log(ex);
-                  });
-                
-                  break;
-            
-            case "twitter" :
-                axios
-                .get(
-                "http://127.0.0.1:5000/get_processed_data_twitter?brand=" +
-                    brand
-                )
-                .then((res) => {
-                    return callBack(null,res)
-                  })
-                .catch((ex) => {
-                console.log(ex);
-                });
-            
+    switch (source) {
+      case "yelp":
+        axios
+          .get(
+            "http://127.0.0.1:5000/get_processed_data_yelp?brand=" +
+              brand +
+              "&location=arizona"
+          )
+          .then((res) => {
+            return callBack(null, res);
+          })
+          .catch((ex) => {
+            console.log(ex);
+          });
 
-                break;
-               
-            case "reddit" : 
-                axios
-                .get(
-                "http://127.0.0.1:5000/get_processed_data_reddit?brand=" +
-                    brand
-                )
-                .then((res) => {
-                return callBack(null,res)
-                })
-                .catch((ex) => {
-                console.log(ex);
-                });
-                
+        break;
 
-                break;
+      case "twitter":
+        axios
+          .get(
+            "http://127.0.0.1:5000/get_processed_data_twitter?brand=" + brand
+          )
+          .then((res) => {
+            return callBack(null, res);
+          })
+          .catch((ex) => {
+            console.log(ex);
+          });
 
-        }
-      
+        break;
 
+      case "reddit":
+        axios
+          .get("http://127.0.0.1:5000/get_processed_data_reddit?brand=" + brand)
+          .then((res) => {
+            return callBack(null, res);
+          })
+          .catch((ex) => {
+            console.log(ex);
+          });
 
-
-          
-    },
-
-    
-
-
-
-}
+        break;
+    }
+  },
+};
--- backend/models/twittermodel.js
+++ backend/models/twittermodel.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var twitterSchema = new Schema({
-    _id : {type:Schema.ObjectId,auto:true},
-    brand : {type : String, required :true},
-    source : {type : String,required : true},
-    negativeSentences : [{type:String}],
-    positiveSentences : [{type : String}],
-    requestId : {type : String, required : true},
-    timeStamp:{type: Date, required : true},
-    topicWiseRatings : [{topic : {type:String},rating : {type:Number}}]
-})
-module.exports = mongoose.model('twitterreviews',twitterSchema);
+  _id: { type: Schema.ObjectId, auto: true },
+  brand: { type: String, required: true },
+  source: { type: String, required: true },
+  negativeSentences: [{ type: String }],
+  positiveSentences: [{ type: String }],
+  requestId: { type: String, required: true },
+  timeStamp: { type: Date, required: true },
+  topicWiseRatings: [{ topic: { type: String }, rating: { type: Number } }],
+  // LineGraphPolarityDates : [{negative : [{type: String }], neutral : [{type: String }],  positive : [{type: String }]],
+  donutSentimentCounts: [
+    { polarity: { type: Number }, reviewCount: { type: Number } },
+  ],
+});
+module.exports = mongoose.model("twitterreviews", twitterSchema);
